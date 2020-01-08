cdaf31f5c2d9b713a9657719aa204308ce0db627
prusovak2 <prusova.k2@seznam.cz>
2020-01-08 13:48:37 +0100

    final commit

--- myShell/stable/CSTYLEMyCd.c
+++ myShell/stable/CSTYLEMyCd.c
@@ -86,6 +86,7 @@ int Cd(char *dir) {
     // error while chdir(), errno is set
     warn("cd: %s", dir);
     DEBUG_PRINT("error message based on errno should be appended\n");
+    free(dirBeforeChange);
     return 1;
   } else {
     // set coresponding PWD and OLDPWD
