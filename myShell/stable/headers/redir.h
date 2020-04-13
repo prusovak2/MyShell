a30142b5f87739e2928b7e5bdadb7e8d540ef6b3
prusovak2 <prusova.k2@seznam.cz>
2020-04-13 16:59:03 +0200

    actually commiting redirections

---
+++ myShell/stable/headers/redir.h
@@ -0,0 +1,9 @@
+ #ifndef MY_REDIR_H
+ #define MY_REDIR_H //to prevent double declaration of stuff
+ 
+ #include "types.h"
+ #include <sys/types.h>
+
+void redir(REDIR * redirP);
+
+#endif
---
+++ myShell/stable/redir.c
@@ -0,0 +1,59 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <err.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#include "debugPrint.h"
+#include "types.h"
+#include "redir.h"
+
+void redir(REDIR * redirP)
+{
+    //REDIRECTION
+    if(redirP->input != NULL)
+    {   
+        DEBUG_PRINT_YELLOW("redirecting stdin to %s\n", redirP->input);
+        int fd;            
+        if ((fd = open(redirP->input, O_RDONLY)) == -1)
+        {
+            err(1,"%s: error while opening the file",redirP->output);
+            //warnx("%s: no such file",redirP->input);
+            //return or exit
+        }
+        DEBUG_PRINT_YELLOW("< opened sucessfully\n");   
+        close(0); //close stdin
+        dup(fd);  //duplicate file fd to stdin
+        close(fd); //duplicated fd no longer needed
+    }
+    if(redirP->output!=NULL)
+    {
+        DEBUG_PRINT_YELLOW("redirecting stdout to %s\n", redirP->output);
+        int fd;
+        if(redirP->append)
+        {
+            DEBUG_PRINT_YELLOW("Append\n");
+            if ((fd = open(redirP->output, O_WRONLY | O_CREAT | O_APPEND, 0666)) == -1)
+            {
+                err(1,"%s: error while opening the file",redirP->output);
+                    //warnx("%s: error while opening the file",redirP->output);
+                    //exit(1);
+            }
+            DEBUG_PRINT_YELLOW(">> opened sucessfully\n");                  
+        }
+        else
+        {
+            if ((fd = open(redirP->output, O_WRONLY | O_CREAT | O_TRUNC, 0666)) == -1)
+            {
+                err(1,"%s: error while opening the file",redirP->output);
+                    //warnx("%s: error while opening the file",redirP->output);
+                    //exit(1);
+            } 
+            DEBUG_PRINT_YELLOW("> opened sucessfully\n");     
+        }
+        close(1); //close stdout
+        dup(fd); //duplicate file fd to stdin
+        close(fd); //duplicated fd no longer needed               
+    }
+}
